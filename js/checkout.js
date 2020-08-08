47fa095f1908e1d46054f895685710796e81b264
cedricchaffard <cedric.chaffard@gmail.com>
2020-08-08 09:14:00 +0200

    Dynamic number of Id per order line

--- js/checkout.js
+++ js/checkout.js
@@ -32,10 +32,10 @@ function addLine(key, name, choosenColor, quantity, formatPrice) {
     // const span = document.createElement('span');
     // span.setAttribute('class', 'orderQuantity')
     // span.innerHTML = quantity
-    
+
     cartContainer.innerHTML +=
-    
-    `<td>${name}</td>
+
+        `<td>${name}</td>
     <td>${choosenColor}</td>
     <td>
     <span class="orderQuantity">
@@ -44,29 +44,50 @@ function addLine(key, name, choosenColor, quantity, formatPrice) {
     <button class="orderQuantityPlus">+</button>
     </span>
     <td>${formatPrice}</td>
-    <td id="total-price">${totalPricePerTeddy}</td>`
+    <td id="total-price" class="sum-per-teddy">${totalPricePerTeddy}</td>`
 
     document.querySelector('.orderQuantityPlus').addEventListener('click', () => addOne(key));
     document.querySelector('.orderQuantityMinus').addEventListener('click', () => substractOne(key));
 
-    // if (quantity === 0) {
-    //     var element = document.querySelector('td');
-    
-    //     var parent = element.parentNode;
-    //     parent.removeChild(element);
-    
-    //     console.log(quantity)
-    // }
-    
-    const totalOfOrder = document.querySelector('#total-order');
+    let list = document.getElementsByClassName("sum-per-teddy");
+    for (var i = 0; i < list.length; i++) {
+        list[i].setAttribute("id", "total-price" + i);
 
-    totalOfOrder.innerHTML +=
-
-    `<td>${totalPricePerTeddy}</td>`
-
-    console.log(totalPricePerTeddy)
+    }
+    
 }
 
+for (let i in cart) {
+
+    let totalPerTeddies = cart[i].quantity * parseFloat(cart[i].price) / 100;
+    let totalPricePerTeddy = parseInt(new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(totalPerTeddies));
+
+    console.log(totalPricePerTeddy)
+
+}
+
+let totalOfOrder = document.querySelector('#total-order');
+
+let amountOne = document.getElementsByTagName("td");
+
+// // let amount2 = document.querySelector('#total-price1');
+
+// // totalOfOrder = amount1 + amount2
+
+console.log(amountOne)
+
+for (var i = 0; i < amountOne.length; i++) {
+
+    console.log(amountOne);
+
+    console.log(amountOne)
+}
+
+//     totalOfOrder = document.querySelector('#total-price') * totalPricePerTeddy
+
+// console.log(amountOne)
+
+// totalOfOrder.innerHTML = amountOne
 
 
 // Prévoir le cas ou le panier est vide (afficher un message)
